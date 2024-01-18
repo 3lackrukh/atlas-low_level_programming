acd08fe4378f7a1e7f25313807e7b470d64d6070
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-18 02:32:10 +0000

    Betty is a bitch

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description; this is a simple program that prints a message
+ * using the puts function.
+ *
+ * Return value for main is always 0 (success)
+ */
 int main(void)
 {
+	/** 
+	 * This code prints a message using the puts function.
+	 */
+
 	puts("\"Programming is like building a multilingual puzzle");
+
+	/** 
+	 * Return value is always 0 signifying success.
+	 */
 
 	return (0);
 }
