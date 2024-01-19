8ee6d3c2756222b66c0bd3e465dc57855fd532e4
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-19 18:37:31 +0000

    header io added, no else argument

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main - entry point of the program
+ *
+ * Description: This is a random integer generator that prints
+ * the integer and states whether it is positive, zero, or
+ * negative.
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+{
+		printf("%d is positive\n", n);
+}
+	else if (n == 0)
+{
+		printf("%d is zero\n", n);
+}
+	else
+{
+		printf("%d is negative\n", n);
+}
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,4 @@
+Exercises with char, int, and unsigned int variables. 
+- declaring variable types and assigning values
+- using if, if ... else, and while loops
+
