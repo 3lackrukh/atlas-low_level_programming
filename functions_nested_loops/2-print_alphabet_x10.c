a5bb33dec704246ff0d487b6bea35e062c42c6ec
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-21 21:47:00 +0000

    Now I'm nesting.

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * Description: this program prints the alphabet, in lowercase,
+ * followed by ainewline using only _putchar.
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <=9; i++)
+{
+		for (c = 'a'; c <= 'z'; c++)
+{
+                _putchar(c);
+}
+        _putchar('\n');
+}
+}
