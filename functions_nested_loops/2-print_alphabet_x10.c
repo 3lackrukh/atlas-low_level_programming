0b31c2ed1b86a6672de6144f908ee3b4170c01e8
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-22 17:43:16 +0000

    documented for grandmas

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * Description: this program uses a nested for loop
- * to print the alphabet, in lowercase,
+ * print_alphabet_x10 - prints the alphabet, in lowercase,
  * followed by a newline 10x over.
  *
  * Return: Always 0
@@ -14,12 +13,12 @@ void print_alphabet_x10(void)
 	int i;
 	char c;
 
-	for (i = 0; i <=9; i++)
+	for (i = 0; i <= 9; i++)
 {
 		for (c = 'a'; c <= 'z'; c++)
 {
-                _putchar(c);
+		_putchar(c);
 }
-        _putchar('\n');
+	_putchar('\n');
 }
 }
