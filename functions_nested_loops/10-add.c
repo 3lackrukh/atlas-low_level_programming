5a056affca0bddd4336989a1d11a794c278bb6a5
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-23 04:26:36 +0000

    adding is neat

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: sum of x and y
+ */
+
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}
