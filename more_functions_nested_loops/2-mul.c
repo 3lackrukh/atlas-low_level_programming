2f44e67f79283a0b235582ae3ad555239e0ce1a8
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-24 17:47:40 +0000

    just one line of code?

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul- multiplies two integers
+ *
+ * @a: first integer to be multiplied.
+ * @b: second integer to be multiplied.
+ *
+ * Return: always (0) success!
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+return (res);
+}
