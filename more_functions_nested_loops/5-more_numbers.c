21c5db301cda0f8cd11baaadc3df7f5853105165
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-25 00:39:15 +0000

    quite a chestnut

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers- prints the numbers 0 - 14 followed by a newline
+ * 10 times.
+ *
+ * return: always (0) success.
+ */
+
+void more_numbers(void)
+{
+	int i,j;
+
+	for(i = 0; i < 10; i++)
+	{
+		for(j = 0; j < 15; j++)
+		{
+			if(j > 9)
+			{
+				_putchar('0' + (j / 10));
+			}
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
+}
