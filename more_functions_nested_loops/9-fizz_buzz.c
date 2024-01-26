6681b1e662534456991fb31b1ed8eaa3d5cd99cf
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-26 02:54:47 +0000

    Added == check for equality not comparison. Moved \n out of for loop.

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 
 /**
- * - lists numbers from 1 to 100.
- *   in the case that a number is a multiple of 3, 
- *   "Fizz" will print instead.
- *   
- *   in the case that a number is a multiple of 5, 
- *   "Buzz" will print instead.
+ * main- Entry point of the program.
  *
- *   in the case that a number is a multiple of both 3 and 5,
- *   "FizzBuzz" will print instead.
+ * Description: This program prints numbers from 1 to 100.
+ * in the case that a number is a multiple of 3,
+ * "Fizz" will print instead.
  *
- *   return: always (0). success.
+ * In the case that a number is a multiple of 5,
+ * "Buzz" will print instead.
+ *
+ * In the case that a number is a multiple of both 3 and 5,
+ * "FizzBuzz" will print instead.
+ *
+ * Return: always (0). success.
  */
 
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <101; i++)
-		if ((i % 3 = 0) && (i % 5 = 0))
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-		       printf("FizzBuzz ");
+			printf("FizzBuzz ");
 		}
-		if else (i % 3 = 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		if else (i % 5 = 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else
 		{
-			printf('%d ', i);
+			printf("%d ", i);
 		}
-		printf('\n');
+	}
+	printf("\n");
+return (0);
 }
 
