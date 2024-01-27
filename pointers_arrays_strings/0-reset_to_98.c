2298aadace311110bd9d8afa17c30b83b4277cd2
3lackrukh <nathan.rhys@atlasschool.comls>
2024-01-27 20:36:48 +0000

    specify pointer value

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,14 @@
  * reset_to_98- takes a pointer to an int as a parameter,
  * and updates the value of that int to 98.
  *
- * @*n: the pointer tken as a paremeter.
+ * @n: the pointer tken as a parameter.
  *
  * return: always (0). success.
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
+{
+	int *ptr = n;
 
-
+	*ptr = 98;
+}
