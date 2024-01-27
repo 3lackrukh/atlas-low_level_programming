29ba782367feb07b054b788f79ce4959fc2068be
3lackrukh <nathan.rhys@atlasschool.comls>
2024-01-27 21:33:43 +0000

    drawing board for #2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen- returns the length of a string.
+ *
+ * @s: the string to be checked.
+ *
+ * return: always (0). success. 
+ */
+
+int _strlen(char *s)
+{
+
+	
