18e66e327cd7f0fa934d5b95fb3fd3a3ba06d2bf
3lackrukh <nathan.rhys@atlasschool.comls>
2024-01-27 18:57:53 +0000

    setting up header file and README

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98- takes a pointer to an int as a parameter,
+ * and updates the value of that int to 98.
+ *
+ * @*n: the pointer tken as a paremeter.
+ *
+ * return: always (0). success.
+ */
+
+void reset_to_98(int *n);
+
+
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains project files written to practice the use of pointers, arrays, and strings in the C programming language.
