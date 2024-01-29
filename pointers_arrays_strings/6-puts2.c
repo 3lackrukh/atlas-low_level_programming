a24f8b90b097259a8125675b5f3057ef01488de4
3lackrukh <nathan.rhys@atlasschool.com>
2024-01-29 03:28:30 +0000

    increment i

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2- prints every other character of a string,
+ * beginning with the first character,
+ * ending with a new line.
+ *
+ * @str: the string to be printed from.
+ *
+ * Return: always (0) success.
+ */
+
+void puts2(char *str)
+{
+
+	int i = 0;
+
+	while ((str[i] != '\0') && (str[i] % 2 == 0))
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
