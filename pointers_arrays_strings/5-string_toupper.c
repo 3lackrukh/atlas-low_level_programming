2b8f8a777e69e47cadb9a7d62d584c6f1d4e57a3
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-01 18:58:58 +0000

    pointer named, compilation success, expected output 100%.

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,21 +4,21 @@
  * string_toupper- scans a string for lowercase characters
  * and replaces them with the corresponding uppercase characters.
  *
- * @*: the string pointed to.
+ * @ptr: the string pointed to.
  *
- * Return: the updated string.
+ * Return: ptr the updated string.
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *ptr)
 {
 	int i;
 
-	for (i = 0; *[i] != '\0'; i++)
+	for (i = 0; ptr[i] != '\0'; i++)
 	{
-		if ((*[i] >= 97) && (*[i] <= 122))
+		if ((ptr[i] >= 97) && (ptr[i] <= 122))
 		{
-			*[i] = *[i] - 32;
+			ptr[i] = ptr[i] - 32;
 		}
 	}
-return (*);
+return (ptr);
 }
