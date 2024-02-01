5f3b2481458a107fd1733e470a906cba490d3dc0
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-01 18:20:09 +0000

    to name a pointer or not to name a pointer?

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,36 +1,24 @@
 #include "main.h"
 
 /**
+ * string_toupper- scans a string for lowercase characters
+ * and replaces them with the corresponding uppercase characters.
  *
+ * @*: the string pointed to.
+ *
+ * Return: the updated string.
  */
 
 char *string_toupper(char *)
 {
 	int i;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; *[i] != '\0'; i++)
 	{
-		if ((ptr[i] >= 97) && (ptr[i] <= 122))
+		if ((*[i] >= 97) && (*[i] <= 122))
 		{
-			(ptr[i] + 32);
+			*[i] = *[i] - 32;
 		}
 	}
-
-
-
-
-//har* string_toupper(char *str);
-//
-//char* string_toupper(char *str)
-//	{
-//	        int i = 0;
-//
-//	        while (i != '\0')
-//	        {
-//	                if ((str[i] >= 97) && (str[i] <= 122))
-//	                {
-//	                        (str[i] + 32);
-//	                        i++;
-//	                }
-//	        }
-//
+return (*);
+}
