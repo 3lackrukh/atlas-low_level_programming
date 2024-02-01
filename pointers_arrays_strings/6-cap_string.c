a8ebab60d315118d88e30353ce27277852b1b57f
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-01 22:44:38 +0000

    too many nests?

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,28 @@
 #include "main.h"
 
 /**
- * cap_string- capitalizes all the words of a string. 
+ * cap_string- capitalizes all the words of a string.
  *
  * @str: the string to scan and capitalize.
  *
- * Return: str, the edited string. 
+ * Return: str, the edited string.
  */
 
 char *cap_string(char *str)
 {
 	int i;
+	int j;
+	char sep[12] = {' ', '\t', '\n', ',', ';', '.', '!', '"', '(', ')', '{', '}'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (((str[i] == sep[j]) && ((str[i + 1] >= 97) && (str[i + 1] <= 122))))
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
