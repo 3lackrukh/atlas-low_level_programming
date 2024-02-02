e26159a7ddb307b2a99344eb74a196497630b76b
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-02 05:32:43 +0000

    initialized multidimensional array

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * leet- encodes a string into 1337.
+ *
+ * @p: pointer to the character string to be encoded.
+ *
+ * Return: the pointer (p).
+ */
+
+char *leet(char *p)
+{
+	int i;
+	char array [5][3] = {a, A, 4} , {e, E, 3} , {o, O, 0} , {t, T, 7} , {l, L, 1};
+
+	While (p[i] != '\0')
+
