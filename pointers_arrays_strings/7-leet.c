3626c7f02615bd442e82fc610c3912603795d0c2
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-02 20:04:09 +0000

    i iterates through string, k through indices.

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -3,15 +3,31 @@
 /**
  * leet- encodes a string into 1337.
  *
- * @p: pointer to the character string to be encoded.
+ * @str: pointer to the character string to be encoded.
  *
- * Return: the pointer (p).
+ * Return: the pointer (str).
  */
 
-char *leet(char *p)
+char *leet(char *str)
 {
 	int i;
-	char array [5][3] = {a, A, 4} , {e, E, 3} , {o, O, 0} , {t, T, 7} , {l, L, 1};
+	int k;
+	char array[3][5] = {
+		{'a', 'e', 'o', 't', 'l'},
+		{'A', 'E', 'O', 'T', 'L'},
+		{'4', '3', '0', '7', '1'}
+	};
 
-	While (p[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (k = 0; k < 5; k++)
+		{
+			if (str[i] == array[0][k] || str[i] == array[1][k])
+			{
+				str[i] = array[2][k];
+			}
+		}
+	}
+return (str);
+}
 
