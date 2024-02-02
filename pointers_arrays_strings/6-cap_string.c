de32211516f3ae9a8d253eafeb4e0bf8defc4f03
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-02 04:11:51 +0000

    I hate myself for how many loops I need

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,32 +10,32 @@
 
 char *cap_string(char *str)
 {
-        int i;
-        int j;
-        char sep[12] = {' ', '\t', '\n', ',', ';', '.', '!', '"', '(', ')', '{', '}'};
+	int i;
+	int j;
+	char sep[12] = {' ', '\t', '\n', ',', ';', '.', '!', '"', '(', ')', '{', '}'};
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if ((str[0] >= 97) && (str[0] <= 122))
-                {
-                        str[i] = str[i] - 32;
-                }
-                for (j = 0; sep[j] != '\0'; j++)
-                {
-                        if (str[i] == sep[j])
-                        {
-                                i++;
-                                j = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[0] >= 97) && (str[0] <= 122))
+		{
+			str[i] = str[i] - 32;
+		}
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i] == sep[j])
+			{
+				i++;
+				j = 0;
 				if ((str[i] >= 97) && (str[i] <= 122))
-                                {
-                                        str[i] = str[i] - 32;
-                                }
+				{
+					str[i] = str[i] - 32;
+				}
 				else
 				{
 					for (j = 0; sep[j] != '\0'; j++)
-					{	
+					{
 						if (str[i] == sep[j])
-                        			{
+						{
 							i++;
 							j = 0;
 							if ((str[i] >= 97) && (str[i] <= 122))
@@ -48,5 +48,5 @@ char *cap_string(char *str)
 			}
 		}
 	}
-        return (str);
+	return (str);
 }
