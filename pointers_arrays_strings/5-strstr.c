1338fb38d5e446e8f0237e34e1fa7c9b71e30773
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-05 05:28:07 +0000

    a promising start

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -8,7 +8,19 @@ char *_strstr(char *haystack, char *needle)
     int l;
     
     //iterate through haystack for a match to needle 0
-    //if match
+    //if match save haystack position
+    //iterate i and k 
+
+    //if not match
+    //reset i to 0
+    //check i0 against k
+    //if match save haystack position
+    //iterate i and k
+
+    //when i = \0
+    //return address haystack l
+    
+    // if k = \0 return NULL
     
     for (i = 0; needle[i] != '\0'; i++)
     {
