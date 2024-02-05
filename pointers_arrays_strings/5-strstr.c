95db0e34e8b3d52f074d24005ad406c8643ac008
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-05 19:16:11 +0000

    added match found boolean

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,42 +1,37 @@
 #include "main.h"
 
+/**
+ * _strstr- locates a substring.
+ *
+ * @haystack: the string to be scanned.
+ *
+ * @needle: the substring to be scanned for.
+ *
+ * Return: pointer to first character if substring found.
+ * if not, NULL.
+ *
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i;
-    int k = 0;
-    int l;
-    
-    //iterate through haystack for a match to needle 0
-    //if match save haystack position
-    //iterate i and k 
+	int i, k, l, n;
+	int match;
 
-    //if not match
-    //reset i to 0
-    //check i0 against k
-    //if match save haystack position
-    //iterate i and k
-
-    //when i = \0
-    //return address haystack l
-    
-    // if k = \0 return NULL
-    
-    for (i = 0; needle[i] != '\0'; i++)
-    {
-        while (haystack[k] != '\0')
-        {
-            if (needle[i] == haystack[k])
-            {
-                l = k;
-                i++;
-                k++;
-            }
-            else
-            {
-              k++;
-            }
-        }
-    }
-return (NULL);
+	while (needle[l] != '\0')
+	{
+		l++;
+	}
+	for (k = 0; haystack[k] != '\0'; k++)
+	{
+		match = 0;
+		for (i = 0; i < l; i++)
+	{
+		if (needle[i] == haystack[k])
+		{
+			match = 1;
+			n = k;
+		}
+	}
+	}
+return (match ? &haystack[n - l] : NULL);
 }
