84699cfb43a892f79285ae291638959942ecd2eb
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-07 17:26:59 +0000

    pass the pointer not the character pointed to

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * _puts_recursion- prints a string, followed by  a new line. 
+ * _puts_recursion- prints a string, followed by  a new line.
  *
- * @*s: pointer to the first char in the string to be printed.
+ * @s: the string to be printed.
  *
  * Return: nothing.
  */
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
 
 	if (*s == '\0')
 	{
 		_putchar ('\n');
-		return (0);
-	{
+	}
 	else
+	{
 		_putchar (*s);
 		s++;
-		return _puts_recursion(*s);
+		(_puts_recursion(s));
 	}
 }
