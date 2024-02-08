e69da5834b166bab1efabf833b5cb52385b783e4
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-08 20:54:02 +0000

    three conditions

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _pow_recursion- returns the value of one integer raised to the power
+ * of another integer.
+ *
+ * @x: the integer to be raised.
+ *
+ * @y: the power raised to. 
+ *
+ * Return: the raised value of x. If y is lower than 0, -1.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y > 0)
+	{
+		return (x * (_pow_recursion(x, y-1)));
+	}
+	else
+	{
+		return (1);
+	}
+}
