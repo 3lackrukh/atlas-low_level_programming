ed296cbd6c1dd568854bfaa3aa8c907e27e30afb
3lackrukh <155496966+3lackrukh@users.noreply.github.com>
2024-02-10 15:15:18 -0600

    Delete static_libraries directory

--- static_libraries/README.md
+++
@@ -1,2 +0,0 @@
-This folder contains static libraries of several C functions I have
-created for projects so far. 
--- static_libraries/create_static_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Compiling $(find . -name '*.c') to object files."
-gcc -c $(find . -name '*.c')
-
-echo "Compiling $(find . -name '*.o') into liball.a."
-ar rc liball.a $(find . -name '*.o')
