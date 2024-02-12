ff9c22e0555d37cdd622fcf34e9796b809a73f82
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-12 02:51:25 +0000

    atoi strings 1 and 2

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main- entry point of thr program.
+ * prints the product of two integers. In the case that two integers
+ * are not passed in, prints ERROR.
+ *
+ * @argc: the number of arguments passed in.
+ *
+ * @argv: a vector of command line strings passed in.
+ *
+ * Return: 0 for success, 1 for error
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int p;
+
+	if (argc >= 2)
+	{
+		p = (atoi(argv[1]) * (atoi(argv[2])));
+		printf("%d\n", p);
+	}
+	else
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+return (0);
+}
