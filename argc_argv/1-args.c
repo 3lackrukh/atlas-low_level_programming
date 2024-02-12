450ae6fee27ed9b510d8a18b4c529fe1793bc05d
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-12 01:43:15 +0000

    argc -1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- entry point of the program. prints the number of arguments
+ * passed into it.
+ *
+ * @argc: the number of items in argv.
+ *
+ *@argv: vector of command line strings passed in.
+ *
+ * Return: always 0 (success).
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
