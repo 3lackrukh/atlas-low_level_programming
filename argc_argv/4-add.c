f4ac384d4ddea4c19aadd75bd5d90f03e1379fa7
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-12 04:48:03 +0000

    added helper function _isdigit, int j iterates through string of chars before atoi converts accepted strings to int for addition.

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,9 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * main- entry point of thr program.
- * prints the product of two integers. In the case that two integers
- * are not passed in, prints ERROR.
+ * _isdigit- Check if a character is a digit (0-9).
+ *
+ * @c: the character to be checked.
+ *
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
+
+int _isdigit(char *c)
+{
+	if ((*c >= '0') && (*c <= '9'))
+	{
+		return (1);
+	}
+	return (0);
+}
+/**
+ * main- entry point of the program.
+ * prints the sum of input integers. If non-integers passed in,
+ * prints Error. If nothing passed in, prints 0.h
  *
  * @argc: the number of arguments passed in.
  *
@@ -14,21 +31,22 @@
 
 int main(int argc, char *argv[])
 {
-	int i, s;
+	int i, j; 
+	int s = 0;
 
 	if (argc >= 3)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) != 0)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				s += atoi(argv[i]);
+				if (_isdigit(&argv[i][j]) != 1)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			s += atoi(argv[i]);
 		}
 		printf("%d\n", s);
 	}
