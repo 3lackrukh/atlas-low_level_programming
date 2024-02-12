1abd168dfb4f1aea67a8713272409884e158624f
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-12 01:31:46 +0000

    print argv[0]

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * whatsmyname- prints its name, followed by a newline. 
+ *
+ * @argc: the number of items in argv.
+ *
+ *@argv[]: vector of command line strings, wherin lies the name to be
+ * printed.
+ *
+ * Return: always 0 (success).
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,2 @@
+This folder contains programs written to practice mastering the use of argc
+and argv. 
