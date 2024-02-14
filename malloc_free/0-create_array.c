fa6adf1eb3bf62fbc11d6f56e045e09b5df4e261
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-14 17:32:26 +0000

    don't get too free

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,7 +2,30 @@
 #include "main.h"
 
 /**
+ * create_array- creates an array of chars, and initializes it with
+ * a specified char.
  *
+ * @size: an integer specifying the size of the array.
+ *
+ * @c: the char to be used to initialize the array.
+ *
+ * Return: a pointer to the array, or NULL if it fails.
  */
 
 char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i = 0;
+
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+return (str);
+}
