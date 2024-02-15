98abd49b2fba372a3306ce71cc51faa76c98acf5
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-15 20:01:59 +0000

    this feels too easy. Iterate through each array, free each slot, then free the grid pointer.

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,8 +2,22 @@
 #include "main.h"
 
 /**
+ * free_grid- frees memory space holding a specified 2d grid.
  *
+ * @grid: addressof the grid.
+ *
+ * @height: the height of the grid.
+ *
+ * Return: nothing. you lose!
  */
 
 void free_grid(int **grid, int height)
 {
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
