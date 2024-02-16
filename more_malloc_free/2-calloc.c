5c71b9a6a539f0eb9a8b10db97ec6fcdae9ae519
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-16 19:59:22 +0000

    allocate for n * unsigned int

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	p = malloc(nmemb * size * sizeof(unsigned int));
+	p = malloc(nmemb * sizeof(unsigned int));
 	if (p == NULL)
 	{
 		return (NULL);
