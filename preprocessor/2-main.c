82cc27bdeef48855bf106222d82b27ade916152e
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-19 02:19:39 +0000

    simple print f

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- entry point of the program.
+ * prints the name of the file it was compiled from.
+ *
+ * Return: 0 success.
+ */
+
+int main(void)
+{
+	printf("02-main.c\n");
+	return (0);
+}
