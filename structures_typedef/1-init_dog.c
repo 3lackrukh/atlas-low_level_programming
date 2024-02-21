e3dda38b6d5a86c470b0a8a9feeb7989742862d0
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-21 16:33:16 +0000

    Now betty compliant, and NULL protected!

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -4,13 +4,26 @@
 #include "main.h"
 
 /**
+ * init_dog- a function that initializes a variable of the dog struct.
+ * 
+ * @d: pointer to the beginning of the struct.
  *
+ * @name: pointer to the string containing name.
  *
+ * @age: a float containing the age of the dog.
+ *
+ * @owner: a pointer to the string containing the caretaker's name.
+ *
+ * Return: nothing.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
+	if (d == NULL)
+	{
+		return (NULL)
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
