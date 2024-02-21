e5379ab93a23c6d0d8966395e6137bffd9d0a178
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-21 15:47:45 +0000

    dog struct attempt 1

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+/**
+ * struct dog- a data structure for storing information about good
+ * doggos.
+ * @name: a char string containing the dogdog's name.
+ * @age: a float describing the dog's age... probably in years.
+ * @owner: a char string containing the name of their caretaker. 
+ *
+ */
+
+typedef struct dog{
+
+char *name;
+float age;
+char *owner;
+} dog;
+
+#endif
