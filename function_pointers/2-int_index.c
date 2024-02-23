392ab3fafcb34bd22c2cc0fb9d16bbb79483f2e0
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-23 23:02:08 +0000

    iterate through the array, if true, return position i

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index- a function passing an array into the function called
+ * and returning the index position of the first true evaluation.
+ *
+ * @array: pointer to the first element of the array to be evaluated.
+ *
+ * @size: an integer specifying the size of the array.
+ *
+ * @cmp: pointer to the function to be called.
+ *
+ * Return: the array position of the first true evaluation of the
+ * array, if there are none or if size is 0 or less, -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	while (i < size)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (-1);
+}
