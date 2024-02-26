183715592c2a8b9edad35c192473a1ad077a825a
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-26 21:24:24 +0000

    saving my place to go home

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers- prints numbers separated by a specified character.
+ *
+ * @separator: the string to be printed between integers.
+ *
+ * @n: the number of integers to be printed.
+ *
+ * Return: nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i = 0;
+
+	va_start(nums, n);
+
+	while (i < n)
+	{
+		if (separator == NULL)
+		{
+			printf("%s ", (va_arg(nums, int));
+			i++;
+		}
+		else
+		{
+			printf("%s%c ", (va_arg(nums, int), separator));
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(nums);
+}
