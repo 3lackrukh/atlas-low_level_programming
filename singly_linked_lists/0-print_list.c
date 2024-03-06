b7c9d7a9c6a621b1443a11bad70087611fe71969
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-06 19:33:36 +0000

    Betty compliant formatted

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 
 /**
+ * print_list- prints all the elements of a list_t list.
  *
+ * @h: pointer to the head of a singly linked list_t list.
+ *
+ * Return: the number of elements in the linked list.
  */
 
 size_t print_list(const list_t *h)
@@ -9,20 +13,20 @@ size_t print_list(const list_t *h)
 	int element_count = 0;
 
 	if (h == NULL)
-		return(element_count);
+		return (element_count);
 
 	if (h->str == NULL)
 	{
-		printf ("[0] (nil)\n");
+		printf("[0] (nil)\n");
 		element_count++;
 		h = h->next;
 	}
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		element_count++;
 		h = h->next;
 	}
-	return(element_count);
+	return (element_count);
 }
