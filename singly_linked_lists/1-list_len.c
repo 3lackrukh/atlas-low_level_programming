e7900a72cbcc7f622ba311eccde260b87e987363
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-06 20:16:56 +0000

    copied and stripped down print_list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len- counts the number of elements in a linked list_t list.
+ *
+ * @h: pointer to the head of a singly linked list_t list.
+ *
+ * Return: the number of elements in the linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+        int element_count = 0;
+
+        if (h == NULL)
+                return (element_count);
+
+        while (h != NULL)
+        {
+                element_count++;
+                h = h->next;
+        }
+        return (element_count);
+}
