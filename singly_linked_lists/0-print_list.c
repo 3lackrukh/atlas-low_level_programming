df675d200367c94ad1102feed71e1254fa59ec73
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-06 18:43:37 +0000

    singly_linked_lists directory set up. Header file set up with prototypes

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+	int element_count = 0;
+
+	if (h->str == NULL)
+	{
+		printf ("[0] (nil)\n");
+		element_count++;
+		h = h->next;
+	}
+
+	while(h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		element_count++;
+		h = h->next;
+	}
+	return(element_count);
+}
