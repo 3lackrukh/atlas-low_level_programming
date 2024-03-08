ca817dfb083936cb508a3835b78958e0ad88f638
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-08 18:13:22 +0000

    same as 4-free_listint.c, but make sure to derefernce the pointer to the pointer to head. Effectively modifying head to NULL.

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2- scrubs through a singly linked list
+ * freeing allocated memory and sets the head pointer
+ * to NULL.
+ *
+ * @head: pointer to the pointer to the first position
+ * of the singly linked listint_t list.
+ *
+ * Return: nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
