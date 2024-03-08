361f0910e323ce9693d88b9f99e3bdf07fe96469
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-08 20:15:53 +0000

    Betty style documentation added

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,14 @@
 #include "lists.h"
 
 /**
+ * get_nodeint_at_index- returns the nth node of a listint_t linked list.
  *
+ * @head: pointer to the first node in the linked list.
+ *
+ * @index: an integer specifying the node we're looking for.
+ *
+ * Return: a pointer to the requested node. if the list is empty, or the
+ * requested node does not exist, returns NULL.
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
