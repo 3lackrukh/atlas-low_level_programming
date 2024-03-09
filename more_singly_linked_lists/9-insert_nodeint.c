36c488904fdbc7c2710d5375ada0395b04e5e2e2
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-09 05:34:47 +0000

    blank line after declarations. line 24 parenthesis around return value. line 29 space removed before ++

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,15 @@
 #include "lists.h"
 
 /**
+ * insert_nodeint_at_index- inserts a new node at a given position.
  *
+ * @head: pointer to a pointer to the beginning of a linked list.
+ *
+ * @idx: the index position at which the new node is to be added.
+ *
+ * @n: integer data to be contained in the new node. 
+ *
+ * Return: the address of the new node. or NULL if failure.
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -9,6 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int position = 0;
 	listint_t *temp = *head;
 	listint_t *new_node = malloc(sizeof(listint_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -21,12 +30,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return *head;
+		return (*head);
 	}
 	while (temp != NULL && position < (idx - 1))
 	{
 		temp = temp->next;
-		position ++;
+		position++;
 	}
 	if (position == (idx - 1))
 	{
