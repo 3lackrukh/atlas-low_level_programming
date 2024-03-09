4f6465a7cf6cfcf46ac77ea27b12c43b8aacc463
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-09 03:23:15 +0000

    declare listint_t temp. If head is not NULL set temp to head, add temp->n to sum, set temp to temp->next. return sum.

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint- scrubs through a singly linked list
+ * summing stored integers.
+ *
+ * @head: pointer to the beginning of the linked list.
+ *
+ * Return: the sum of n values in each node.
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t temp;
+	int sum = 0;
+
+	while(head !=  NULL)
+	{
+		temp = head;
+		sum = (sum +(temp->n));
+		head = head->next;
+	}
+	return (sum);
+
+}
