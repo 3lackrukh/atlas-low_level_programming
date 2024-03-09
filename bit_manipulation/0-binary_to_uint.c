60cfed6f7709532d9bf237819ab2fa8d086dcac1
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-09 20:19:43 +0000

    added _strlen function

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _strlen- returns the length of a string.
+ *
+ * @s: the string to be checked.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(const char *s)
+{
+	const char *p = s;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+
+	return (p - s);
+}
+
+/**
  * binary_to_uint- converts a number to an unsigned int.
  *
  * @b: the binary number handed in as a char string.
@@ -12,7 +32,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int d = 0;
-	int len = strlen(b);
+	int len = _strlen(b);
 
 	if (b == NULL)
 	{
@@ -20,7 +40,7 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (i = 0; i < len; i++)
 	{
-		if ((b[i] != '1') && (b[i] != '0'))
+		if ((b[i] != '1') &&(b[i] != '0'))
 		{
 			return (0);
 		}
