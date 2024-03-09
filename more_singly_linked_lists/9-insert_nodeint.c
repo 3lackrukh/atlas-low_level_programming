ffa5fc1a1cba5b5969690e842886c2df43a8831e
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-09 05:22:08 +0000

    set *temp to *head at declaration. allocate mem for new_node at declaration. if temp == NULL or idx 0, set new node to pos 0.

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -6,31 +6,33 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp;
-	listint_t *new_node;
 	unsigned int position = 0;
-
-	new_node = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 
-	if (head != NULL)
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (temp == NULL || idx == 0)
 	{
-		*temp = *head;
-		while (temp != NULL && position < idx)
-		{
-			temp = temp->next;
-			position ++;
-		}
-		if (position == idx)
-		{
-			new_node = temp;
-			new_node->n = n;
-			new_node->next = temp->next;
-			return (new_node);
-		}
+		new_node->next = *head;
+		*head = new_node;
+		return *head;
+	}
+	while (temp != NULL && position < idx)
+	{
+		temp = temp->next;
+		position ++;
+	}
+	if (position == idx)
+	{
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (new_node);
 	}
 	return (NULL);
 }
