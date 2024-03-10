52b3d6fa6ab30602e61465cfdd913f794eca5ca7
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-10 03:09:34 +0000

    allocate memory for 16 char string. while n > 0, use & operator to divide. store remainder in string, n>>=1. reverse through string printing to str[0]. free str.

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	int remainder;
+	char *str = malloc(sizeof(char ) * 16);
+
+	if (str == NULL)
+	{
+		return;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (i = 0; n > 0; i++)
+	{
+		remainder = n & 1;
+		str[i] = '0' + remainder;
+		n >>= 1;
+	}
+
+	while (i > 0)
+	{
+		i--;
+		_putchar(str[i]);
+	}
+	free(str);
+}
