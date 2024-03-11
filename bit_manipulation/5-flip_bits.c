a7bd387c01ab71ea94a0d24763cd0fd3a643df53
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-11 01:43:32 +0000

    result = the XOR comparison. while the result is not 0, compare the last digit to one. If they match, count++ shift the result to the right and compare again!

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flip_bits- compares the binary values of two integers and
+ * counts the number of bits which would need to flip to get
+ * from one to the other.
+ *
+ * @n: the first number to be compared.
+ *
+ * @m: the second number to be compared.
+ *
+ * Return: the number of bits to flip.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result;
+	int count = 0;
+
+	result = (n ^ m);
+	while (result);
+	{
+		count += result & 1;
+		result >>= 1;
+	}
+	return count;
+}
