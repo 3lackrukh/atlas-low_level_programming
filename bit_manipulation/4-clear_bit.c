050ee0fd154ae3302d9d9d553534aa22ec99acf7
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-11 00:59:55 +0000

    indent with tabs instead of spaces

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -13,13 +13,13 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        unsigned long int mask = 1;
+	unsigned long int mask = 1;
 
-        if (index > 63)
-        {
-                return (-1);
-        }
-        mask <<= index;
-        *n &= ~mask;
-        return (1);
+	if (index > 63)
+	{
+		return (-1);
+	}
+	mask <<= index;
+	*n &= ~mask;
+	return (1);
 }
