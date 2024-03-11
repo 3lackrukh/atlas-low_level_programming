463062beac0ce91474a9627419b14d2847f72df6
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-11 21:00:39 +0000

    malloc a buffer sizeof letters + 1. NULL terminate char *buff. iterate through buffer and _putchar till \0. close fd and free buff.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, i;
+	ssize_t bytes_read;
+
+	char *buff = malloc(sizeof(char) * (letters + 1));
+	if (buff == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+	
+	fd = open (filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	bytes_read = read(fd, buff, letters);
+	
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
+	buff[bytes_read] = '\0';
+
+	for (i = 0; buff[i] != '\0'; i++)
+		_putchar(buff[i]);
+
+	close(fd);
+	free (buff);
+
+	return (bytes_read);
+}
