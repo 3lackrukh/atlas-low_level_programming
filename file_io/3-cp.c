37299e036342f84bb3423261c422e067019dc34a
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-12 05:11:56 +0000

    this project was RUDE!

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,33 +8,38 @@
 /**
  *
  */
+int main (int argc, char *argv[])
+{
 
+}
 int cp_file(const char *file_from, const char *file_to)
 {
 	int fr_fd, to_fd;
 	mode_t mode = S_IRUSR | S_IWUSR;
-	ssize_t chars_rd, chars_wr;
+	ssize_t chars_rd;
+	/* ssize_t chars_wr;
+	 */
 	char buffer[1024];
 
 	fr_fd = open(file_from, O_RDONLY);
 	if (fr_fd == -1)
 	{
-		dprintf("Error: Can't read from file %c\n" , file_from);
+		printf("Error: Can't read from file %s\n" , file_from);
 		return (98);
 	}
 
 	to_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (to_fd == -1)
 	{
-		dprintf("Error: Can't write to %c\n" , file_to);
+		printf("Error: Can't write to %s\n" , file_to);
 		close(fr_fd);
 		return (99);
 	}
 
 	while((chars_rd = read(fr_fd, buffer, sizeof(buffer))) > 0)
-	{
+	/*{
 		chars_wr = write(to_fd, buffer, chars_rd);
-	}
+	}*/
 	close(fr_fd);
 	close(to_fd);
 	return (1);
