e2bf7bdca4b0255380cf5e6bfc892463c8f69600
3lackrukh <155496966+3lackrukh@users.noreply.github.com>
2024-03-13 03:03:16 +0000

    Betty coding style changes throughout.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,45 +14,45 @@ int cp_file(const char *file_from, const char *file_to)
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	ssize_t chars_rd;
 	ssize_t chars_wr;
-	
+
 	char buffer[1024];
 
 	fr_fd = open(file_from, O_RDONLY);
 	if (fr_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n" , file_from);
-		exit (98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
 
 	to_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (to_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n" , file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		if (close(fr_fd) == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n" , file_from);
-			exit (100);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_from);
+			exit(100);
 		}
 
-		exit (99);
+		exit(99);
 	}
 
-	while((chars_rd = read(fr_fd, buffer, sizeof(buffer))))
+	while ((chars_rd = read(fr_fd, buffer, sizeof(buffer))))
 	{
 		if (chars_rd == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n" , file_from);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			if (close(fr_fd) == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %s\n" , file_from);
-				exit (100);
+				dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_from);
+				exit(100);
 			}
 			if (close(to_fd) == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %s\n" , file_to);
-				exit (100);
+				dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_to);
+				exit(100);
 			}
-			exit (98);
+			exit(98);
 		}
 		chars_wr = write(to_fd, buffer, chars_rd);
 		if (chars_wr != chars_rd)
@@ -60,27 +60,27 @@ int cp_file(const char *file_from, const char *file_to)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			if (close(fr_fd) == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %s\n" , file_from);
-				exit (100);
+				dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_from);
+				exit(100);
 			}
 			if (close(to_fd) == -1)
 			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %s\n" , file_to);
-				exit (100);
+				dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_to);
+				exit(100);
 			}
-			exit (99);
+			exit(99);
 		}
 
 	}
 	if (close(fr_fd) == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n" , file_from);
-			exit (100);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_from);
+			exit(100);
 		}
 	if (close(to_fd) == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n" , file_to);
-			exit (100);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_to);
+			exit(100);
 		}
 	return (0);
 }
