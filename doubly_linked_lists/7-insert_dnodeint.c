3b43cac9d23482a01a62d75470e4b1f63e82ac65
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-15 16:39:24 +0000

    initialize new node n. new node prev and next set to null. find index position if exists. nn prev = temp prev. nn next = temp. temp->prev-> next = nn. temp->prev = nn.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,11 +20,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (*h == NULL)
 		return (NULL);
 
-	temp = *h
+	temp = *h;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
+	while (temp != NULL && i < idx)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (i != idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	
+	new_node->prev = temp->prev;
+	new_node->next = temp;
+	if (temp->prev != NULL)
+		temp->prev->next = new_node;
+	temp->prev = new_node;
+
+	return (new_node);
 }
