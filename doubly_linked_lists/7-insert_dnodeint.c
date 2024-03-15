e2a0c31d4b6f3ef29eef5d0f8607fea84e6a9099
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-15 18:22:11 +0000

    removed initializations for new next and prev. added helper function dlistint_len.

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,29 +23,48 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return(add_dnodeint(h, n));
 
+	if (idx == dlistint_len(*h))
+		return (add_dnodeint_end(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	
 	temp = *h;
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
 	while (temp != NULL && i < idx)
 	{
 		temp = temp->next;
 		i++;
 	}
-	if (i != idx)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	
 	new_node->prev = temp->prev;
-	new_node->next = temp;
 	temp->prev->next = new_node;
 	temp->prev = new_node;
+	new_node->next = temp;
 
 	return (new_node);
+}
+
+/**
+ * dlistint_len- counts and returns the number of elements
+ * in a doubly-linked list.
+ *
+ * @h: pointer to the head node of the list.
+ *
+ * Return: the number of elements.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
