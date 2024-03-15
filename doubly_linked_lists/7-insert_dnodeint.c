a7afebf021114e113e133f67a9c917390237b555
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-15 17:20:04 +0000

    if idx = 0, returns a call to add_dnodeint(h, n).

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,8 +17,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *temp, *new_node;
 
-	if (*h == NULL)
+	if (*h == NULL && idx != 0)
 		return (NULL);
+
+	if (idx == 0)
+		return(add_dnodeint(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -27,14 +30,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp = *h;
 	new_node->n = n;
 	new_node->prev = NULL;
-
-	if (i == idx)
-	{
-		new_node->next = temp;
-		temp->prev = new_node;
-		return (new_node);
-	}
-
 	new_node->next = NULL;
 	while (temp != NULL && i < idx)
 	{
