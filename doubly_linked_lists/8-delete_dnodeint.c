4cc0e18f615c1e58cee61236d310d71af9a191da
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-16 21:34:18 +0000

    corrected 2nd to last if statement

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -38,7 +38,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	if (temp->next != NULL)
-		temp->prev->next = temp->next;
+		temp->next->prev = temp->prev;
 
 	if (temp->prev != NULL)
 		temp->prev->next = temp->next;
