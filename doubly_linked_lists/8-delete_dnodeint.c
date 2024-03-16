b946789f06eedf4e8946e937997514c1d84ba9b9
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-16 20:45:28 +0000

    check if list is empty. If index is 0set prev to NULL, free temp. traverse to the node before index. check for index out of bounds. update next node's prev, update prev node's next. Free temp and return 0.

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index- deletes the node at the index
+ * position of a doubly linked list.
+ *
+ * @head: pointer to a pointer to the first element of the
+ * list.
+ *
+ * @index: the specified position of the node to be deleted.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *temp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+
+	while (temp != NULL && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+		return (-1);
+
+	if (temp->next != NULL)
+		temp->prev->next = temp->next;
+
+	if (temp->prev != NULL)
+		temp->prev->next = temp->next;
+
+	free(temp);
+	return (1);
+}
