b0e3da91135e8d2e25e883d44fbbf871d7466ac9
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-19 02:50:36 +0000

    traced memory leak to hash_table_set. free new_node in the case a key match is found.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,22 +17,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int index;
 	hash_node_t *new_node, *current_node;
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL || ht == NULL || key == NULL)
-	{
-		free(new_node);
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
 	index = key_index((unsigned char *)key, ht->size);
 	current_node = ht->array[index];
 	while (current_node != NULL)
 	{
 		if (strcmp(current_node->key, key) == 0)
 		{
+			free(new_node);
 			free(current_node->value);
 			current_node->value = strdup(value);
 			if (current_node->value == NULL)
 				return (0);
+			current_node = current_node->next;
 			return (1);
 		}
 		current_node = current_node->next;
--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -11,21 +11,17 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *current, *next;
+	hash_node_t *current;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		while (ht->array[i] != NULL)
 		{
 			current = ht->array[i];
-			while (current != NULL)
-			{
-				next = current->next;
-				free(current->key);
-				free(current->value);
-				free(current);
-				current = next;
-			}
+			ht->array[i] = ht->array[i]->next;
+			free(current->key);
+			free(current->value);
+			free(current);
 		}
 	}
 	free(ht->array);
