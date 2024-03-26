2110798e2133f95f09effa1782923289bfbb6ac7
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-26 02:52:26 +0000

    in main replaced newline char with null byte in buffer string after getline call. in forkcecute commented out exit on success of child process.

--- Exercises/10-simple_shell.c
+++ Exercises/10-simple_shell.c
@@ -4,15 +4,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-typedef struct cmd_args
+char **tokenize(char *buffer, char *delimiter)
 {
-	char *cmd;
-	char **args;
-} cmd_args;
-
-cmd_args tokenize(char *buffer, char *delimiter)
-{
-	cmd_args result = {NULL, NULL};
 	int str_ct = 0, i = 0, l = 0;
 	unsigned long int k;
 	char **array = NULL;
@@ -24,24 +17,15 @@ cmd_args tokenize(char *buffer, char *delimiter)
 			str_ct++;
 	}
 	str_ct++;
-	
+
 	array = (char **)malloc(sizeof(char *) * (str_ct + 1));
 	if (array == NULL)
 	{
 		perror("tokenize malloc array failure");
-		return (result);
+		return (NULL);
 	}
 	
 	portion = strtok(buffer, delimiter);
-	result.cmd = strdup(portion);
-	if (result.cmd == NULL)
-	{
-		perror("Tokenize strdup cmd failure");
-		free(array);
-		return (result);
-	}
-	i++;
-	portion = strtok(NULL, delimiter);
 	while (portion != NULL)
 	{
 		array[i] = strdup(portion);
@@ -51,28 +35,28 @@ cmd_args tokenize(char *buffer, char *delimiter)
 			for (l = 0; l < i; l++)
 				free(array[l]);
 			free(array);
-			free(result.cmd);
-			return (result);
+			return (NULL);
 		}
 		i++;
 		portion = strtok(NULL, delimiter);
 	}
 	array[i] = NULL;
-	result.args = array;
-	return (result);
+	return (array);
 }
-int forkcecute(cmd_args cmd_ln) 
+
+int forkcecute(char **cmd_ln)
 {
 	pid_t pid;
+	/* char *args[]= {"ls", "-l", "/tmp", NULL};*/
 	char *envp[] = {NULL};
 
 	/* Check for empty arg array */
-	if (cmd_ln.cmd == NULL) /*|| cmd_ln->args == NULL)*/
+	if (cmd_ln == NULL || cmd_ln[0] == NULL)
 	{
 		perror("forkcecute cmd_ln empty");
 		return (-1);
 	}
-	
+
 	pid = fork();
 	if (pid == -1)
 	{
@@ -82,14 +66,14 @@ int forkcecute(cmd_args cmd_ln)
 	else if (pid == 0)
 	{
 		/* Child process */
-		printf("Child process %d executing %s\n", getpid(),cmd_ln.cmd);
-		if (execve(cmd_ln.cmd, cmd_ln.args, envp) == -1)
+		printf("Child process %d executing %s\n", getpid(),cmd_ln[0]);
+		if (execve(cmd_ln[0], cmd_ln, envp) == -1)
 		{
 			perror("execve failed");
 			exit(EXIT_FAILURE);
 		}
-		exit(EXIT_SUCCESS);
-        }
+		/* exit(EXIT_SUCCESS); */
+	}
 	else
 	{
 		/* Parent process */
@@ -99,24 +83,24 @@ int forkcecute(cmd_args cmd_ln)
 	}
 	return 0;
 }
-
 int main ()
 {
 	char *buffer;
-	cmd_args cmd_ln = {NULL, NULL};
+	char **cmd_ln = NULL;
 	size_t buffsize = 4095;
 	ssize_t bytes_read;
 
 	buffer = (char *)malloc(sizeof(char) * buffsize);
 	if (buffer == NULL)
 		return (-1);
-
+	
 	while (1)
 	{
 		printf("$ ");
 		bytes_read = getline(&buffer, &buffsize, stdin);
 		if (bytes_read == 1)
 			continue;
+		buffer[bytes_read - 1] = '\0';
 		if (strncmp(buffer, "exit", 4) == 0)
 			break;
 		cmd_ln = tokenize(buffer, " ");
