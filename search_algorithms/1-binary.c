157afd1c095ca4fa3fb286378a8a6dae3737cdeb
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-10 22:05:36 +0000

    changed spaces to tabs in 1-binary.c

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -9,7 +9,7 @@
  *
  * @value: the integer to be searched for.
  *
- * Retrun: the index position where value has been located, else -1.
+ * Return: the index position where value has been located, else -1.
  */
 
 int binary_search(int *array, size_t size, int value)
@@ -47,15 +47,15 @@ int binary_search(int *array, size_t size, int value)
  */
 void print_array(const int *array, size_t size)
 {
-        size_t i;
+	size_t i;
 
-        i = 0;
-        while (array && i < size)
-        {
-                if (i > 0)
-                        printf(", ");
-                printf("%d", array[i]);
-                ++i;
-        }
-        printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
